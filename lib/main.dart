bcef5e8e28e0e2a64c6173328288d1de5133fc8c
joyel124 <geolrco124@hotmail.com>
2023-10-14 18:37:34 -0500

    feat: Added newAppName.

--- lib/main.dart
+++ lib/main.dart
@@ -48,18 +48,21 @@ class _MatrixLUState extends State<MatrixLU> {
   late List<List<double>> a;
   late List<List<double>> l;
   late List<List<double>> u;
+  late List<List<double>>? p;
+  bool isValid = false;
   _MatrixLUState() {
     n = 5; // Puedes establecer el valor inicial de 'n' aquí
     a = List.generate(n, (i) => List.generate(n, (j) => 0.0));
     l = List.generate(n, (i) => List.generate(n, (j) => 0.0));
     u = List.generate(n, (i) => List.generate(n, (j) => 0.0));
+    p = List.generate(n, (i) => List.generate(n, (j) => 0.0));
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Factorización LU',
+          'FactoLU',
           style: TextStyle(
             color: backgroundColor, // Texto blanco
             fontWeight: FontWeight.w700, // Semi-Bold
@@ -86,16 +89,19 @@ class _MatrixLUState extends State<MatrixLU> {
                 ),
                 onChanged: (value) {
                   setState(() {
+                    isValid = int.tryParse(value) != null;
                     n = int.tryParse(value) ?? 0;
                   });
                 },
               ),
               const SizedBox(height: 12),
               ElevatedButton(
-                onPressed: () {
-                  if (n >=3 && n <= 10) {
+                onPressed: isValid? () {
+                  if (n >=4 && n <= 10) {
                     setState(() {
                       a = generarMatriz(n);
+                      p = permute(a)[1];
+                      a = permute(a)[0];
                       if(esMatrizValida(a)){
                         l = List.generate(n, (i) => List.filled(n, 0.0));
                         u = List.generate(n, (i) => List.filled(n, 0.0));
@@ -136,12 +142,12 @@ class _MatrixLUState extends State<MatrixLU> {
                   }else{
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(
-                        content: Text('El valor debe estar entre 3 y 10'),
+                        content: Text('El valor debe estar entre 4 y 10'),
                         backgroundColor: Colors.red,
                       ),
                     );
                   }
-                },
+                }: null,
                 style: ElevatedButton.styleFrom(
                   primary: primaryColor,
                   onPrimary: Colors.white,
@@ -165,6 +171,18 @@ class _MatrixLUState extends State<MatrixLU> {
                     const SizedBox(height: 12),
                     const Text('Matriz U:', style: labelStyle),
                     MatrixDisplay(matrix: u),
+                  ],
+                ),
+              if (p != null)
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Matriz Pt:', style: labelStyle),
+                    MatrixDisplay(matrix: p!),
+                    // Asumiendo que tienes una función `multiplyMatrices` que
+                    // multiplica dos matrices y devuelve el resultado
+                    const Text('Comprobando: Pt * L * U:', style: labelStyle),
+                    MatrixDisplay(matrix: a),//multiply(multiply(p!, l), u)
                   ],
                 ),
             ],
@@ -241,13 +259,77 @@ bool esMatrizValida(List<List<double>> matriz) {
   }
 
   // Comprobación de ceros en la diagonal principal
-  for (int i = 0; i < n; i++) {
+  /*for (int i = 0; i < n; i++) {
     if (matriz[i][i] == 0) {
       return false;
     }
-  }
+  }*/
 
   return true;
+}
+
+List<dynamic> permute(List<List<double>> matrix) {
+  int n = matrix.length;
+
+  if (esMatrizValida(matrix)) return [matrix, null];
+
+  List<List<double>> pt = List.generate(n, (i) => List.generate(n, (j) => i == j ? 1.0 : 0.0));
+
+  for (var p in permutations(pt)) {
+    List<List<double>> mt = matrix.map((row) => row.map((e) => e.toDouble()).toList()).toList();
+    List<List<double>> m = multiply(p, mt);
+    if (esMatrizValida(m)) {
+      return [m, p];
+    }
+  }
+  return [null, null];
+}
+
+List<List<double>> multiply(List<List<double>> a, List<List<double>> b) {
+  int aRowCount = a.length;
+  int aColumnCount = a[0].length;
+  int bRowCount = b.length;
+  int bColumnCount = b[0].length;
+
+  if (aColumnCount != bRowCount) {
+    throw ArgumentError('Number of columns of Matrix A must be equal to number of rows of Matrix B.');
+  }
+
+  List<List<double>> result = List.generate(aRowCount, (i) => List<double>.filled(bColumnCount, 0));
+
+  for (int i = 0; i < aRowCount; i++) {
+    for (int j = 0; j < bColumnCount; j++) {
+      for (int k = 0; k < aColumnCount; k++) {
+        result[i][j] += a[i][k] * b[k][j];
+      }
+    }
+  }
+
+  return result;
+}
+
+List<List<List<double>>> permutations(List<List<double>> matrix, [int row = 0]) {
+  if (row == matrix.length) {
+    return [matrix];
+  }
+
+  List<List<List<double>>> perms = [];
+  for (int i = row; i < matrix.length; i++) {
+    List<List<double>> newMatrix = matrix.map((r) => List.of(r)).toList();
+    newMatrix[row] = List.from(matrix[i]);
+    newMatrix[i] = List.from(matrix[row]);
+
+    perms.addAll(permutations(newMatrix, row + 1));
+  }
+  return perms;
+}
+
+List<List<double>> identityMatrix(int size) {
+  List<List<double>> id = List.generate(size, (i) => List<double>.filled(size, 0));
+  for (int i = 0; i < size; i++) {
+    id[i][i] = 1;
+  }
+  return id;
 }
 
 double determinante(List<List<double>> matriz) {
